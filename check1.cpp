cdf6a5dcba6693970185f6f6eae3be617d3dd611
atulrana97 <atulrana97@gmail.com>
2020-09-16 11:25:06 +0530

    first commit

---
+++ check1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> one;
+    one.push_back(1);
+    one.push_back(2);
+    int l=one.size();
+    int arr[l]={0};
+    cout<<arr[0];
+    return 0;
+}
